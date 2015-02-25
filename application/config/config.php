2896cf5fb457ed9e5b5e3bdf36aea4873b130b5d
braslongs <geniessed@gmail.com>
2015-02-25 20:54:25 +0100

    gitignore
    ignore

--- application/config/config.php
+++ application/config/config.php
@@ -17,7 +17,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | environments.
 |
 */
-$config['base_url'] = '';
+$config['base_url'] = 'http://127.0.0.1/Dams/github/webdev2015/';
 
 /*
 |--------------------------------------------------------------------------
