c87239d242f6a9b05643795abffb12c2c2d25f44
Adrien De Mey <adriendemey@gmail.com>
2015-02-26 19:48:16 +0100

    Création du modèle M_announcement.php

---
+++ application/models/M_announcement.php
@@ -0,0 +1,7 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+class M_announcement extends CI_Model {
+    function __construct() {
+        parent::__construct();
+    }
+}
+?>
