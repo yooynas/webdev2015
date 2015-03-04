51887b46c6c7f4e5fc289291a7450e1b39b9a4b0
Rouskof <kevin.lessire@gmail.com>
2015-03-04 20:49:04 +0100

    config
    config

--- application/controllers/chapter.php
+++ application/controllers/chapter.php
@@ -6,6 +6,11 @@ class Chapter extends CI_Controller
 		parent::__construct();
 		$this->load->model('M_chapter');
 		$this->load->model('M_module');
+		$this->load->model('M_lessons');
+		if (!$this->session->userdata('id')) 
+		{
+			redirect('auth/login', 'refresh'); 
+		}
 	}
 	public function index()
 	{
@@ -19,12 +24,28 @@ class Chapter extends CI_Controller
 		
 		
 	}
-	public function add_chapter() {
-		$this->load->view('chapter_add_new');
+	public function add_new_chapter() {
+		$data['contenu'] = 'chapter/V_chapter_add_new';
+		$data['lessons'] = $this->M_lessons->get();
+		$this->load->view('templates/base', $data);
 	}
-//	public function module()
-//	{
-//		$data['modules'] = $this->M_chapter->get();
-//	}
+	public function add_chapter() {
+		if (isset($_POST['add_chapter']))
+		{
+			$new_chapter = array
+			(
+		
+			
+			'name_chapter' => $_POST['name_chapter'],
+			'begin_chapter' => $_POST['date_chapter'],
+			'num_chapter' => $_POST['num_chapter'],
+			'fk_lessons_chapter' => $_POST['lessons'],
+			);
+		}
+		 var_dump($new_chapter);
+		 $this->M_chapter->save($new_chapter);
+	}
+	
+
 }
 ?>
