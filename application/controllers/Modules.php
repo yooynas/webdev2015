dc48e2916ffe90fc3bf52c77a0796b493415d585
nicolasmortelette <nicolasmortelette@gmail.com>
2015-03-04 18:41:14 +0100

    update controller modules

--- application/controllers/Modules.php
+++ application/controllers/Modules.php
@@ -5,12 +5,20 @@ class Modules extends CI_Controller
 	{
 		parent::__construct();
 		$this->load->model('M_module');
+        $this->load->controller('chapter');
 	}
+    
+    public function getmodules {
+        $listmodules = $this->M_module->get();
+        
+    }
     
 	public function index()
 	{
-		$get = $this->M_module->get();
-        var_dump($get);
+        
+        $this->  
+		
+        
 	}
 }
 
