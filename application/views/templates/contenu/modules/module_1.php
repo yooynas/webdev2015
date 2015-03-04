fc56353f7699b193e273cd289d3a2feea7075fd4
ProfGoupyl <rudy.renard@ifosupwavre.be>
2015-03-04 19:39:11 +0100

    Ajout des assets hepers
    A ajouter dans votre autoload (helper assets)

---
+++ application/views/templates/contenu/modules/module_1.php
@@ -0,0 +1,13 @@
+<h1>Fonctionnement des sites WEB …</h1>
+<h2>Les bases</h2>
+<ul>
+  <li>De nombreuses personnes confondent (à tort) Internet et le Web. Il faut savoir que le Web fait partie d'Internet.  Internet est donc un grand ensemble qui comprend, entre autres : le Web, les e-mails, la messagerie instantanée, etc.</li>
+    <li>Pour consulter les sites web, nous lançons un un programme appelé navigateur .  Celui-ci se base sur le code HTML et CSS des pages pour savoir ce qu’il doit afficher.</li>
+    <li>Les langages informatiques HTML et CSS permettent de créer des sites web, ils sont incontournables et universels car tous les sites web sont basés sur ces langages.</li> 
+    <li>Le langage HTML a été inventé par un certain Tim Berners-Lee en 1991... Tim Berners-Lee suit encore aujourd'hui avec attention l'évolution du Web. Il a créé le World Wide Web Consortium (W3C) qui définit les nouvelles versions des langages. Il a par ailleurs créé plus récemment la World Wide Web Foundation, qui analyse et suit l'évolution du Web</li>
+</ul>
+<h2>Page Web et source HTML</h2>
+<ul>
+<li>Qu’appelle-t-on « page web » ? Les pages web sont des documents informatiques qui peuvent contenir du texte, des images, des tableaux, des formulaires et d’autres éléments multimédias.</li> 
+<li>Elles sont créées par les webmasters à l’aide des langages HTML et CSS, et sont visualisées par les internautes grâce à un navigateur web. </li>
+</ul>
