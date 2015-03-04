a06df25ee392a45910b1e1fc0dc0b3aa0c822936
ProfGoupyl <rudy.renard@ifosupwavre.be>
2015-03-04 19:49:32 +0100

    mise a jour model chapitre
    Suppression des cochonneries

--- application/models/M_chapter.php
+++ application/models/M_chapter.php
@@ -1,35 +1,4 @@
 <?php
-<<<<<<< HEAD
-class M_chapter extends MY_Model 
-{
-	function get_chapter()
-	{
-		$results = $this->db->get('chapter')->result();
-		return $results;
-	}
-	function get_module()
-	{
-		$results = $this->db->get('modules')->result();
-		return $results;
-	}
-	
-=======
-//class M_chapter extends CI_Model 
-//{
-//	function get_chapter()
-//	{
-//		$results = $this->db->get('chapter')->result();
-//		return $results;
-//	}
-//	function get_module()
-//	{
-//		$results = $this->db->get('modules')->result();
-//		return $results;
-//	}
-//	
-//}
-?>
-<?php
 class M_chapter extends MY_Model {
     function __construct() {
         parent::__construct();
@@ -38,7 +7,5 @@ class M_chapter extends MY_Model {
         $this->primary_key = 'id_chapter';
         $this->table_order = 'id_chapter DESC';
     }
->>>>>>> origin/master
 }
-
 ?>
