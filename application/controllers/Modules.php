7120bfb6d0039824e3b951aa880bd35c69d56fa2
nicolasmortelette <nicolasmortelette@gmail.com>
2015-03-05 18:09:09 +0100

    add  method and models

--- application/controllers/Modules.php
+++ application/controllers/Modules.php
@@ -5,11 +5,16 @@ class Modules extends CI_Controller
 	{
 		parent::__construct();
 		$this->load->model('M_module');
-        //$this->load->model('M_question');
+        $this->load->model('M_question');
 	}
     
     public function getmodules() {
         $listmodules = $this->M_module->get();
+        var_dump($listmodules);
+    }
+    
+    public function getmodulesnum() {
+        $listmodules = $this->M_module->get_key_value('id_module','num_module');
         var_dump($listmodules);
     }
     
@@ -20,15 +25,32 @@ class Modules extends CI_Controller
     }
         
     public function get_questions_by_module() {
-        $this->db->select('label_question');    
-        $this->db->from('questions');
-        $this->db->join('table2', 'table1.id = table2.id');
-        $this->db->join('table3', 'table1.id = table3.id');
+        
+        $this->db->select('*');    
+        $this->db->from('modules');
+        $this->db->join('questions', 'modules.id_module = questions.fk_module_question');
+        //$this->db->join('table3', 'table1.id = table3.id');
         $query = $this->db->get();
+        
+        
+        /*
+        $query = $this->db->query('SELECT num_module, label_question FROM modules INNER JOIN questions ON modules.id_module = questions.fk_module_question');
+        */
+        foreach ($query->result() as $row)
+        {
+            echo $row->num_module;
+            echo "<br>";
+            echo $row->label_question;
+            echo "<br>";
+
+        }
+        
+        echo "bonjour";
+       
     }
     
 	public function index() {        
-        $this->getmodules();  
+        $this->get_questions_by_module();  
 
 	}
 }
