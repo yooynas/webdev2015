70bcb38aa6c81dd038ac935fbff12beb17f4d7f4
omniarchos <omniarchos@outlook.com>
2015-03-05 21:23:01 +0000

    Other conflict resolved

--- application/config/autoload.php
+++ application/config/autoload.php
@@ -87,13 +87,7 @@ $autoload['drivers'] = array();
 |	$autoload['helper'] = array('url', 'file');
 */
 
-<<<<<<< HEAD
-$autoload['helper'] = ['strings','arrays','debug','locale','libraries','url','form'];
-=======
-
-$autoload['helper'] = array('url','form','assets');
->>>>>>> origin/master
-
+$autoload['helper'] = ['strings','arrays','debug','locale','libraries','url','form','assets'];
 
 /*
 | -------------------------------------------------------------------
