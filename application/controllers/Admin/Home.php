f06f791f3302ed2af6113fc95360865261020d0b
Adrien De Mey <adriendemey@gmail.com>
2015-03-11 19:32:12 +0100

    Update gitignore
    + htaccess

--- application/controllers/Admin/Home.php
+++ application/controllers/Admin/Home.php
@@ -1,1 +1,21 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+class Home extends CI_Controller {
+    function __construct() {
+        parent::__construct();
+        /*
+        if ($this->session->userdata('access') != 'admin') {
+            // Test si la personne est loggée en tant qu'admin
+            redirect('auth','refresh');
+        }
+        */
+    }
+    public function Index() {
+        $this->Courses_list();
+    }
+    public function Courses_list() {
+        $this->load->model('M_courses');
+        $data["cours"] = $this->M_courses->get_all();
+        $data["contenu"] = 'admin/home/index';
+        $this->load->view('templates/base', $data);
+    }
+}
---
+++ application/views/templates/contenu/admin/home/index.php
@@ -0,0 +1,8 @@
+<div class="page-header">
+  <h1>EfoSup <small>Administration</small></h1>
+</div>
+<div class="row">
+    <?php
+    var_dump($cours);
+    ?>
+</div>
