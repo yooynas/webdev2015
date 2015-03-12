afb3a152031d44699db16b48e67a716ef5162a28
braslongs <geniessed@gmail.com>
2015-03-12 19:56:21 +0100

    test
    test

--- application/views/admin/contenu/lessons/index.php
+++ application/views/admin/contenu/lessons/index.php
@@ -19,8 +19,8 @@
 	        		<p>Professeur : <?=$lesson->firstname_teacher.' '.$lesson->lastname_teacher?></p>
 	        		<p>
 	        			<a href="<?=site_url("chapter")?>/" class="btn btn-primary" role="button">Voir</a> 
-						<a href="<?=site_url("lessons")?>/edit/<?=$lesson->id_lesson?>" class="btn btn-warning" role="button">Edition</a>
-	        			<a href="<?=site_url("lessons")?>/delete/<?=$lesson->id_lesson?>" class="btn btn-danger" role="button">Supprimer</a>
+						<a href="<?=site_url("admin/lessons")?>/edit/<?=$lesson->id_lesson?>" class="btn btn-warning" role="button">Edition</a>
+	        			<a href="<?=site_url("admin/lessons")?>/delete/<?=$lesson->id_lesson?>" class="btn btn-danger" role="button">Supprimer</a>
 	        		</p>
 	      		</div>
 	    	</div>
