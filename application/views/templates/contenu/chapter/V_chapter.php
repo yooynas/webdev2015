18701f5f033de497b45932133bf381eceee0273d
QuentinMichot <michot.quentin@hotmail.com>
2015-03-26 19:16:25 +0100

    update page cours
    dynamisation de l'affichage des chapitres

--- application/views/templates/contenu/chapter/V_chapter.php
+++ application/views/templates/contenu/chapter/V_chapter.php
@@ -4,33 +4,43 @@
   <div class="panel-heading">Chapitre 1</div>
   <!-- Table -->
   <table class="table chapter">
-      <tr><td><p>1</p></td><td><p>Nom</p></td><td><p>Type</p></td><td><a class="btn btn-default  btn-sm">Suivre</a></td></tr>
-      <tr><td><p>1</p></td><td><p>Nom</p></td><td><p>Type</p></td><td><a class="btn btn-default  btn-sm">Suivre</a></td></tr>
+      <tr>
+          <td><p>1</p></td>
+          <td><p>Nom</p></td>
+          <td><p>Type</p></td>
+          <td><a class="btn btn-default  btn-sm">Suivre</a></td>
+      </tr>
   </table>
   <div class="panel-heading">Chapitre 1</div>
 </div>
+<div class="panel panel-default">
 <?php
 foreach ($chapter as $i)
 {
 ?>
-<div class="num" style="display:inline-block"><?= $i['num_chapter']; ?></div>
-<div class="name" style="display:inline-block"><?= $i['name_chapter']; ?></div>
-<div class="begin"><?= $i['begin_chapter']; ?></div>
+<div class="panel-heading">
+    <div class="name" style="display:inline-block"><?= $i['name_chapter']; ?></div>
+    <div class="begin"><?= $i['begin_chapter']; ?></div>
+</div>
 <?php 
 	foreach ($modules as $module)
 	{
 		if ($i['id_chapter'] == $module['fk_chapter_module'])
 		{
 			?>
-			<p style="margin-left: 50px;">
-			<span class="num"><?= $module['num_module']; ?></span>
-			<a href="<?= site_url('modules/get_theory_by_module/'.$module['id_module']); ?>" class="name" style="padding-left:5px; display:inline-block"><?= $module['name_module']; ?></a>
-            </p>
+			<table class="table chapter">
+			<tr>
+			    <td><p class="num"><?= $module['num_module']; ?></p></td>
+			    <td><p><a href="<?= site_url('modules/get_theory_by_module/'.$module['id_module']); ?>" class="name" style="padding-left:5px; display:inline-block"><?= $module['name_module']; ?></a></p></td>
+                <td><p></p></td>
+                <td><a class="btn btn-default  btn-sm">Suivre</a></td>
+            </table>
 			<?php
 		} 
 		
 	}
 }
 ?>
+</div>
 <div><a href="<?= base_url().'index.php/admin/chapter/add_new_chapter' ?>">Ajouter un nouveaux chapitre</a></div>
 
