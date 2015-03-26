51a9dfd3f550f5421118464f9cb26279e2eb94e5
ProfGoupyl <rudy.renard@ifosupwavre.be>
2015-03-26 20:03:58 +0100

    module 11

--- application/views/templates/contenu/modules/module_11.php
+++ application/views/templates/contenu/modules/module_11.php
@@ -1,6 +1,6 @@
 <h1>Exercice de révision</h1>
-<p>Réaliser la page sur la Croatie.les textes et images vous sont fournis en téléchargement <a href="<?= doc_url("11.zip");?>">ici.</a></p>
-<p>Aperçu final</p>
+<p>Réaliser la page sur la Croatie.<br>Les textes et images vous sont fournis en téléchargement <a href="<?= doc_url("11.zip");?>">ici.</a></p>
+<h3>Aperçu final</h3>
 <div class="container-fluid">
     <div class="row">
         <div class="col-xs-12">
